5dff0712dc31d322c501e3be45b984f840fea34c
Adam Neujahr <adamneujahr@gmail.com>
2019-10-15 09:54:43 -0500

    Started user interface.

--- sweepstakesManager/UserInterface.cs
+++ sweepstakesManager/UserInterface.cs
@@ -8,5 +8,23 @@ namespace sweepstakesManager
 {
     class UserInterface
     {
+        public Contestant GetFirstName(Contestant contestant)
+        {
+            Console.WriteLine("First name: ");
+            contestant.firstName = Console.ReadLine();
+            return contestant;
+        }
+        public Contestant GetLastName(Contestant contestant)
+        {
+            Console.WriteLine("Last name: ");
+            contestant.lastName = Console.ReadLine();
+            return contestant;
+        }
+        public Contestant GetEmail(Contestant contestant)
+        {
+            Console.WriteLine("Email: ");
+            contestant.email = Console.ReadLine();
+            return contestant;
+        }
     }
 }
