60ed0232116c7ab3e1e325458fcd190d3d1761e1
Adam Neujahr <adamneujahr@gmail.com>
2019-10-15 10:59:29 -0500

    Started MainMenu in user interface.

--- sweepstakesManager/UserInterface.cs
+++ sweepstakesManager/UserInterface.cs
@@ -8,6 +8,27 @@ namespace sweepstakesManager
 {
     class UserInterface
     {
+        public void MainMenu()
+        {
+            string select;
+            Console.WriteLine("Sweepstakes Manager\n" +
+                "Press 1 to Input Contestants\n" +
+                "Press 2 to Select a Winner");
+            select = Console.ReadLine();
+            switch (select)
+            {
+                case "1":
+                    // TODO: Input Contestant Process
+                    break;
+                case "2":
+                    // TODO: build out selection process
+                    break;
+                default:
+                    Console.WriteLine("Not valid input.  Please try again.");
+                    MainMenu();
+                    break;
+            }
+        }
         public Contestant GetFirstName(Contestant contestant)
         {
             Console.WriteLine("First name: ");
