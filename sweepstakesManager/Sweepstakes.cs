ade4d4947a6b3b0a225ffa7c38fabcd83c2120bc
Adam Neujahr <adamneujahr@gmail.com>
2019-10-15 11:12:47 -0500

    Started functionality for sweepstakes class.

--- sweepstakesManager/Sweepstakes.cs
+++ sweepstakesManager/Sweepstakes.cs
@@ -8,5 +8,25 @@ namespace sweepstakesManager
 {
     class Sweepstakes
     {
+        string name;
+
+        public Sweepstakes(string name)
+        {
+            this.name = name;
+        }
+
+        void RegisterContestant(Contestant contestant)
+        {
+            //TODO: build out function for registering each contestant to the sweepstakes
+        }
+
+        Contestant PickWinner()
+        {
+            //TODO: build functionality to pick the winner
+        }
+        void PrintContestantInfo(Contestant contestant)
+        {
+            //TODO: functionality to print contestant info.  IE Name, Email, and registration number.
+        }
     }
 }
